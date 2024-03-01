59f98880d4de02652a69c2f7a52aa6e0ea19d3c0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-01 19:58:20 +0000

    Update src/ec2/template.sh

--- src/ec2/template.sh
+++ src/ec2/template.sh
@@ -5,4 +5,4 @@ The tag specification format is
     'ResourceType=launch-template,Tags=[{Key=function,Value=testing}]'
 comment
 
-aws ec2 create-launch-template --launch-template-name {template.name} --version-description {template.version.description} --tag-specifications {tag.specifications} --launch-template-data file://0001/template.json
+aws ec2 create-launch-template --launch-template-name {template.name} --version-description {template.version.description} --tag-specifications {tag.specifications} --launch-template-data file://src/ec2/0001/template.json
