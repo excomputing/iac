1b2798ff441a622b752ab4eec31db7daef039feb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-01 19:55:03 +0000

    Update src/ec2/launch.sh

--- src/ec2/launch.sh
+++ src/ec2/launch.sh
@@ -4,4 +4,4 @@
 {launch.template.identifier}: The identification code of a launch template
 comment
 
-aws ec2 run-instances --user-data file://0001/data.txt --launch-template LaunchTemplateId={launch.template.identifier}
+aws ec2 run-instances --user-data file://src/ec2/0001/data.txt --launch-template LaunchTemplateId={launch.template.identifier}
